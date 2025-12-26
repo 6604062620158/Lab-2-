0f167d02c734b138c30f66c5a69780ad388edf09
Namchai Hangkasee <s6604062620158@email.kmutnb.ac.th>
2025-12-26 21:46:10 +0700

    commit 2 (ws2expr.c)

---
+++ ws2expr.c.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+	
+
+
+	double  a=1,b=1,c=5,d=1;
+	double   A,B,C,D,E;
+	
+	
+	
+	A=((3*(a/5))+(1/b)) ;
+	B=(3*a)+(5*b) / (2+c);
+	C=(2.0/7)*((pow(4,3+c)-(5*d))); 
+	D=sqrt((2+(8*b))/a);
+	E=(pow((b*b)-(4*d),1.0/3.0));
+	
+	
+	printf(" 4.1  A = %.2lf \n",A);
+	printf(" 4.1  B = %.2lf \n",B);
+	printf(" 4.1  C = %.2lf \n",C);
+	printf(" 4.1  D = %.2lf \n",D);
+	printf(" 4.1  E = %.2lf \n",E);
+	
+	
+	a=9, b=2, c=0, d=1;
+	
+		A=((3*(a/5))+(1/b)) ;
+	B=(3*a)+(5*b) / (2+c);
+	C=(2.0/7)*((pow(4,3+c)-(5*d))); 
+	D=sqrt((2+(8*b))/a);
+	E=(pow((b*b)-(4*d),1.0/3.0));
+
+	printf("          \n");
+
+	printf(" 4.2  A = %.2lf \n",A);
+	printf(" 4.2  B = %.2lf \n",B);
+	printf(" 4.2  C = %.2lf \n",C);
+	printf(" 4.2  D = %.2lf \n",D);
+	printf(" 4.2  E = %.2lf \n",E);
+	
+	
+	a=0, b=3, c=3, d=3;
+	
+		A=((3*(a/5))+(1/b)) ;
+	B=(3*a)+(5*b) / (2+c);
+	C=(2.0/7)*((pow(4,3+c)-(5*d))); 
+	D=sqrt((2+(8*b))/a);
+	E=(pow((b*b)-(4*d),1.0/3.0));
+
+	printf("          \n");
+
+	printf(" 4.3  A = %.2lf \n",A);
+	printf(" 4.3  B = %.2lf \n",B);
+	printf(" 4.3  C = %.2lf \n",C);
+	printf(" 4.3  D = %.2lf \n",D);
+	printf(" 4.3  E = %.2lf \n",E);
+	
+}
